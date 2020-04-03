da05a350dd89157ef14b589d3f607d75fbe3dcc6
Dmytro Borovych <s18963@pjwstk.edu.pl>
2020-04-03 13:14:34 +0200

    tut net nichego

---
+++ Controllers/EnrollmentController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using tut3.Models;
+
+namespace tut3.Controllers
+{
+    [Route("api/enrollment")]
+    [ApiController]
+    public class EnrollmentController : Controller
+    {
+        [HttpPost]
+        public IActionResult AddStudent(Student student)
+        {
+            return View();
+        }
+    }
+}
